4e22173e72d84dfc2f9d770b06e001e77be9c3a6
Ijeoma Ugochinyere Eze <ijeomaeze@Ijeomas-MacBook-Pro-2.local>
2026-06-03 18:09:24 -0500

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,18 @@
 const js = require('@eslint/js')
+const globals = require('globals')
 
 module.exports = [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
     rules: {
       'no-unused-vars': 'error',
       'no-console': 'warn',
       'eqeqeq': 'error'
     }
   }
-] 
+]
