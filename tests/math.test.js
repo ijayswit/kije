df15f7a0d92a7a1ba38173f9b08786cf0625ecab
Ijeoma Ugochinyere Eze <ijeomaeze@Ijeomas-MacBook-Pro-2.local>
2026-06-03 19:41:40 -0500

    Add Jest tests to delibrately fail

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,7 @@
 const { add, subtract, multiply, divide } = require('../src/math.js')
 
 test('should add two numbers correctly', () => {
-    expect(add(2, 3)).toBe(5)
+    expect(add(2, 3)).toBe(99)
 })
 
 test('should subtract two numbers correctly', () => {
